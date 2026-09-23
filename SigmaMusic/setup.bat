e5d552f89c26c6f2c93de4523f18cd08520ef7cc
AnAverageBox <brayanel@udel.edu>
2026-09-23 13:19:53 -0400

    added a setup batch file

---
+++ SigmaMusic/setup.bat
@@ -0,0 +1,7 @@
+echo "==============================="
+echo "Installing CustomTkinter..."
+echo "==============================="
+pip3 install -r SigmaMusic/requirements.txt
+echo "==================================================="
+echo "Installation complete! You can now run the app."
+echo "==================================================="
