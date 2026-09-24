99cb0ef409078b590b8e8c482c606880b3b3f39a
AnAverageBox <brayanel@udel.edu>
2026-09-24 18:14:56 -0400

    Added some comments to the UI file for some clarification

--- SigmaMusic/sigma_ui.py
+++ SigmaMusic/sigma_ui.py
@@ -1,15 +1,15 @@
 import customtkinter as ctk
 
 
-def main() -> None:
-    root = ctk.CTk()
-    root.title("Sigma Music")
+def main() -> None: #main function to open the window
+    root = ctk.CTk() #creating the application window (root of all the app)
+    root.title("Sigma Music") 
     root.geometry("900x600")
     root.minsize(500, 300)
     root.configure(fg_color="#f5f5f5")
-    root.mainloop()
+    root.mainloop() #keeps the window open; otherwise would just exit right after opening
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #only not true important if this file is imported into another
     main()
 
